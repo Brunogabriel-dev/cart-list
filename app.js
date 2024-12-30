5f001442d686f2cdeb21f38004e80439749a5df4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-12-30 13:46:49 -0300

    Inclusão de texto

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ let iconCart = document.querySelector('.icon-cart');
 let closeCart = document.querySelector('.close');
 let body = document.querySelector('body');
 let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('.icon-cart span');
 
 let listProducts = [];
+let carts = [];
 
 iconCart.addEventListener('click', () => {
   body.classList.toggle('showCart')
@@ -39,6 +42,9 @@ listProductHTML.addEventListener('click', (event) => {
   }
 })
 
+const addToCart = (product_id) => {
+
+}
 
 const initApp = () => {
   // get data from json
